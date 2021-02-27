e7e479266dc6f362f73b079edc5a4f930591a8d1
kishor <iamkishor1308@gmail.com>
2021-02-27 12:22:56 +0530

    sign in page with social site buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,71 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign in'),
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.teal,
       ),
       body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('images/background image.jpg'))),
         alignment: Alignment.center,
+        padding: EdgeInsets.all(10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
               'Welcome',
               style: TextStyle(
-                color: Colors.redAccent,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
                 fontSize: 40,
               ),
             ),
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Email'),
+              ),
+            ),
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password'
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text(
+                  'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Text(
+              "Don't Have An Account?",
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Colors.yellow,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Image(
+                  image: AssetImage('images/sing up with google button.png'),
+                )),
             TextButton(
               onPressed: () {},
-              child: Text(
-                'Sign In With Google',
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+              child: Image(
+                image: AssetImage('images/fb login inage.png'),
               ),
-            )
+            ),
           ],
         ),
       ),
