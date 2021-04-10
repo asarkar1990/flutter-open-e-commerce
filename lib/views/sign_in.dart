8a4186bff23683b22c435991e2e2c9ae754a6bb4
Arindam Sarkar <asarkar1990@gmail.com>
2021-04-10 19:55:50 +0530

    signin update and added screenshoots

--- lib/views/sign_in.dart
+++ lib/views/sign_in.dart
@@ -4,19 +4,28 @@ import 'package:opencommerce/controllers/auth_controller.dart';
 class SignInView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            body: Container(
-      child: Column(
-        children: [
-          ElevatedButton(
-            child: Text("Sign In With Google"),
-            onPressed: () {
-              signInWithGoogle();
-            },
-          )
-        ],
-      ),
-    )));
+    return Scaffold(
+        body: Container(
+          color: Colors.white70,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Free Commerce",
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                Text("E-commerce Application"),
+                SizedBox(height: 90.0,),
+                ElevatedButton(
+                  child: Text("Sign In With Google"),
+                  onPressed: () {
+                    signInWithGoogle();
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
